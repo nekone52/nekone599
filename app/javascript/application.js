32234b3636da8006d3327b413f9d76a28847b73c
ねこね52 <xicunqinghua5@gmail.com>
2026-03-24 19:20:08 +0900

    編集

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,2 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
-// Bootstrap のインポート(必要に応じて)
-import * as bootstrap from "bootstrap"
+import * as bootstrap from "bootstrap/dist/js/bootstrap"
