4ee15de2802c6ad7338b826ca71f7c9e54faf59e
ねこね52 <xicunqinghua5@gmail.com>
2026-03-24 18:21:35 +0900

    マイグレーションを知ろう

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// Bootstrap のインポート(必要に応じて)
+import * as bootstrap from "bootstrap"
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_043310) do
+ActiveRecord::Schema[7.0].define(version: 2026_03_24_080951) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tel"
+    t.string "address"
   end
 
 end
